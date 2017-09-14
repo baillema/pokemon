fac943bf32751fc413bce91fc977bdfe3a8e63f0
Baille <mathieu.baille@gmail.com>
2017-09-14 08:57:26 +0200

    Update mvn + add java for elasticsearch

--- ecom/ejbs/src/main/java/ElasticSearch.java
+++ ecom/ejbs/src/main/java/ElasticSearch.java
@@ -1,71 +1,38 @@
-
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
-import org.elasticsearch.action.admin.indices.analyze.AnalyzeRequest;
-import org.elasticsearch.action.admin.indices.analyze.AnalyzeResponse;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.bytes.BytesArray;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.script.Script;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.metrics.cardinality.CardinalityAggregationBuilder;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.suggest.SuggestBuilder;
-import org.elasticsearch.search.suggest.SuggestBuilders;
-import org.elasticsearch.search.suggest.completion.CompletionSuggestionBuilder;
-import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
-import org.elasticsearch.xpack.notification.email.EmailTemplate;
-import org.elasticsearch.xpack.watcher.actions.email.EmailAction;
-import org.elasticsearch.xpack.watcher.client.WatchSourceBuilders;
-import org.elasticsearch.xpack.watcher.client.WatcherClient;
-import org.elasticsearch.xpack.watcher.condition.ScriptCondition;
-import org.elasticsearch.xpack.watcher.input.search.SearchInput;
-import org.elasticsearch.xpack.watcher.support.search.WatcherSearchTemplateRequest;
-import org.elasticsearch.xpack.watcher.transport.actions.delete.DeleteWatchResponse;
-import org.elasticsearch.xpack.watcher.transport.actions.put.PutWatchResponse;
-import org.elasticsearch.xpack.watcher.trigger.TriggerBuilders;
-import org.elasticsearch.xpack.watcher.trigger.schedule.Schedules;
-
-
-/**
- * @author Baille Mathieu
- * ElasticSearch
- */
 
 public class ElasticSearch {
+    //Max number of element displayed
+    int size = 10;
 
     TransportClient client;
 
-    //Generic highlight builder
-    //HighlightBuilder highlightBuilder = new HighlightBuilder().field("text");
-
-    public Search()
+    /**
+     * Constructor for the Search class
+     */
+    public ElasticSearch()
     {
         Settings settings = Settings.builder()
-                .put("cluster.name", "elasticsearch-engine")
-                //.put("xpack.security.user", "elastic:changeme")
-                .build();
+                                    .put("cluster.name", "pokelastic")
+                                    .build();
         client = new PreBuiltXPackTransportClient(settings);
         try {
-            client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("152.77.78."), 9300));
+            client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
         } catch (UnknownHostException e) {e.printStackTrace();}
+    }
+
+    /**
+     * Function to get all elements in the pokemons index
+     * @return response of the search request in json
+     */
+    public String matchAll()
+    {
+        SearchResponse response = client.prepareSearch("pokemons")
+                                        .setQuery(QueryBuilders.matchAllQuery())
+                                        .setSize(size)
+                                        .get();
+        return response.toString();
     }
 }
