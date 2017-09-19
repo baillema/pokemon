d8dd4939935a77ad9350ee45c960d9bced2a016e
Unknown <mathieulegras0@gmail.com>
2017-09-19 20:09:24 +0200

    Fix style

--- ecom/web/webapp/src/app/component/items/items.component.ts
+++ ecom/web/webapp/src/app/component/items/items.component.ts
@@ -1,8 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-
-declare var jquery: any;
-declare var $: any;
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-items',
@@ -12,35 +9,9 @@ declare var $: any;
 
 export class ItemsComponent implements OnInit {
 
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
-
-  checkSelected(id: string) {
-    const checkbox = $('#' + id);
-    const label = $(`label[for="` + id + `"]`);
-    if (checkbox.is(':checked') === false) {
-      label.find('img').addClass('selected');
-    } else {
-      label.find('img').removeClass('selected');
-    }
-  }
-
-  toggleItemsGrid() {
-    const grid = $('#itemGrid');
-    if (grid.hasClass('grid')) {
-      grid.removeClass('grid').addClass('line');
-      grid.find('.col-md-4').each(function(){
-        $(this).removeClass('col-md-4').addClass('col-md-12');
-      });
-    } else if (grid.hasClass('line')) {
-        grid.removeClass('line').addClass('grid');
-        grid.find('.col-md-12').each(function(){
-          $(this).removeClass('col-md-12').addClass('col-md-4');
-        });
-    }
   }
 
 }
