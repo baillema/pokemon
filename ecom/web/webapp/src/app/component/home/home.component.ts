341eff9fb9286fb5617c8cda9a1683cc57bd1bde
Brice Varini <varinibrice@gmail.com>
2017-09-19 17:55:01 +0200

    PROTO design cart icon

--- ecom/web/webapp/src/app/component/home/home.component.ts
+++ ecom/web/webapp/src/app/component/home/home.component.ts
@@ -12,7 +12,6 @@ export class HomeComponent {
   constructor() { }
 
   toggleBlock(){
-
     const toggleBlock = document.getElementsByClassName('toggle-block');
     for (let i = 0 ; i < toggleBlock.length; i ++){
       toggleBlock[i].classList.toggle('closed');
