380fd45c10ec914964dd3f534a77fd14afced81a
Brice <varinibrice@gmail.com>
2017-09-19 13:59:37 +0200

    PROTO create userLogin page

--- ecom/web/webapp/src/app/app.module.ts
+++ ecom/web/webapp/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+
 import {AppComponent} from './app.component';
 import {ItemsComponent} from './component/items/items.component';
-import {AppRoutingModule} from "./app-routing.module";
+import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './component/home/home.component';
 import {UsersComponent} from './component/users/users.component';
 import {ItemComponent} from './component/item/item.component';
@@ -14,6 +15,7 @@ import {CartComponent} from './component/cart/cart.component';
 import {CartService} from './service/cart/cart.service';
 import {AngularFontAwesomeModule} from 'angular-font-awesome/angular-font-awesome';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,8 +25,10 @@ import {AngularFontAwesomeModule} from 'angular-font-awesome/angular-font-awesom
     ItemComponent,
     ProfilsComponent,
     AjoutComponent,
+
     EchangeComponent,
     CartComponent
+
   ],
   imports: [
     BrowserModule,
