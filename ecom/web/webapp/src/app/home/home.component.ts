c5e2c8c2638a92e192cf4cfbfb21acb511b8d686
Brice Varini <varinibrice@gmail.com>
2017-09-19 08:16:39 +0200

    PROTO script on Homepage

--- ecom/web/webapp/src/app/home/home.component.ts
+++ ecom/web/webapp/src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  toggleBlock(){
+    alert('click');
+    $('.toggle-block').remove();
   }
-
 }
