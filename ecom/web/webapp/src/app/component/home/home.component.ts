b51f03ff56385f22c931892347e8716a339ec852
Brice <varinibrice@gmail.com>
2017-09-19 13:23:08 +0200

    PROTO ts on Homepage

--- ecom/web/webapp/src/app/component/home/home.component.ts
+++ ecom/web/webapp/src/app/component/home/home.component.ts
@@ -12,7 +12,11 @@ export class HomeComponent {
   constructor() { }
 
   toggleBlock(){
+
     const toggleBlock = document.getElementsByClassName('toggle-block');
-    console.log(toggleBlock);
+    for (let i = 0 ; i < toggleBlock.length; i ++){
+      toggleBlock[i].classList.toggle('closed');
+    }
+
   }
 }
