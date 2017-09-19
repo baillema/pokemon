96728a7887775f86f4c7b88c74d4df7ae4274a96
Brice <varinibrice@gmail.com>
2017-09-19 09:39:30 +0200

    PROTO new component organisation

--- ecom/web/webapp/src/app/app-routing.module.ts
+++ ecom/web/webapp/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ItemsComponent} from './items/items.component';
-import {HomeComponent} from './home/home.component';
-import {UsersComponent} from './users/users.component';
-import {AjoutComponent} from './ajout/ajout.component';
-import {ItemComponent} from './item/item.component';
-import {ProfilsComponent} from './profils/profils.component';
-import {EchangeComponent} from './echange/echange.component';
+import {ItemsComponent} from './component/items/items.component';
+import {HomeComponent} from './component/home/home.component';
+import {UsersComponent} from './component/users/users.component';
+import {AjoutComponent} from './component/ajout/ajout.component';
+import {ItemComponent} from './component/item/item.component';
+import {ProfilsComponent} from './component/profils/profils.component';
+import {EchangeComponent} from './component/echange/echange.component';
 
 // routes
 const appRoutes: Routes = [
--- ecom/web/webapp/src/app/app.module.ts
+++ ecom/web/webapp/src/app/app.module.ts
@@ -2,14 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ItemsComponent} from './items/items.component';
+import {ItemsComponent} from './component/items/items.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ItemComponent } from './item/item.component';
-import { ProfilsComponent } from './profils/profils.component';
-import { AjoutComponent } from './ajout/ajout.component';
-import { EchangeComponent } from './echange/echange.component';
+import { HomeComponent } from './component/home/home.component';
+import { UsersComponent } from './component/users/users.component';
+import { ItemComponent } from './component/item/item.component';
+import { ProfilsComponent } from './component/profils/profils.component';
+import { AjoutComponent } from './component/ajout/ajout.component';
+import { EchangeComponent } from './component/echange/echange.component';
 
 @NgModule({
   declarations: [
