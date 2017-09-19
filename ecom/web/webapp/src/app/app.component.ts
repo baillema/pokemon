cec401e145c0836c3ca611ac42df468d21ed788a
Unknown <mathieulegras0@gmail.com>
2017-09-19 20:14:17 +0200

    Fix style

--- ecom/web/webapp/src/app/app.component.ts
+++ ecom/web/webapp/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { Router} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
-  selector : 'app-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
 })
 
 
 export class AppComponent {
 
-  constructor (private router : Router){
+  constructor(private router: Router) {
 
   }
-  title   = 'PokeShop';
+  title = 'PokeShop';
 }
 
 /*
--- ecom/web/webapp/src/app/app.module.ts
+++ ecom/web/webapp/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AppComponent} from './app.component';
-import {ItemsComponent} from './component/items/items.component';
-import {AppRoutingModule} from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './component/items/items.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './component/home/home.component';
 import { UsersComponent } from './component/users/users.component';
 import { ItemComponent } from './component/item/item.component';
 import { ProfilsComponent } from './component/profils/profils.component';
 import { AjoutComponent } from './component/ajout/ajout.component';
 import { EchangeComponent } from './component/echange/echange.component';
-import {CartComponent} from "./component/cart/cart.component";
-import {CartService} from "./service/cart/cart.service";
+import { CartComponent } from './component/cart/cart.component';
+import { CartService } from './service/cart/cart.service';
 
 @NgModule({
   declarations: [
