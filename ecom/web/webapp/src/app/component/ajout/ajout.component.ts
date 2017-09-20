7caf4698df658cd8962a6e5e886fdcfd5dfad672
Brice <varinibrice@gmail.com>
2017-09-20 16:24:21 +0200

    PROTO add navtab on pokelist

--- ecom/web/webapp/src/app/component/ajout/ajout.component.ts
+++ ecom/web/webapp/src/app/component/ajout/ajout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-ajout',
@@ -12,4 +14,10 @@ export class AjoutComponent implements OnInit {
   ngOnInit() {
   }
 
+  showPane(e: string){
+    $('.list-group-item.active').removeClass('active');
+    $('#' + e + 'Item').addClass('active');
+    $('.tab-pane.active').removeClass('active');
+    $('#' + e).addClass('active');
+  }
 }
