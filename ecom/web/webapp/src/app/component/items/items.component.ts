52a9f50967c2193d4fa65eaf28a431e2d1b7d309
Brice <varinibrice@gmail.com>
2017-09-21 13:19:13 +0200

    PROTO add pretty selection on Items

--- ecom/web/webapp/src/app/component/items/items.component.ts
+++ ecom/web/webapp/src/app/component/items/items.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-items',
@@ -9,9 +12,20 @@ import { Router } from '@angular/router';
 
 export class ItemsComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  checkSelected(id: string) {
+    const checkbox = $('#' + id);
+    const label = $(`label[for="` + id + `"]`);
+    if (checkbox.is(':checked') === false) {
+      label.find('img').addClass('selected');
+    } else {
+      label.find('img').removeClass('selected');
+    }
   }
 
 }
