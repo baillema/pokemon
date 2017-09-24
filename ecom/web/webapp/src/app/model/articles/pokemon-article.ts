811d916ec3b2ec98f96086b1c328690353c07a15
Unknown <mathieulegras0@gmail.com>
2017-09-24 14:44:53 +0200

    Add PokemonArticle model

---
+++ ecom/web/webapp/src/app/model/articles/pokemon-article.ts
@@ -0,0 +1,12 @@
+export interface PokemonArticle {
+  id: number;
+  price: number;
+  name: string;
+  description: string;
+  userId: number;
+  cartId: number;
+  pokemonId: number;
+  level: number;
+  shininess: boolean;
+  state: string;
+}
