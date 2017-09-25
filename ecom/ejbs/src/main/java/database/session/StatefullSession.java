50c6ad1ab8de2b2aae129d3eec05a5f9ac57efb9
Ferrafiat <jeromeferrafiat@alpha-playing.fr>
2017-09-25 10:25:37 +0200

    mise à jour des sessions pour user

--- ecom/ejbs/src/main/java/database/session/StatefullSession.java
+++ ecom/ejbs/src/main/java/database/session/StatefullSession.java
@@ -1,26 +1,131 @@
 package database.session;
 
+import database.entities.CartsEntity;
+import database.entities.FeedbackEntity;
+import database.entities.RolesEntity;
+import database.entities.UsersEntity;
+import database.entities.articles.PokemonsArticlesEntity;
 import database.session.StatefullSessionInterface;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.String;
+import java.util.Set;
 import javax.ejb.Stateful;
 
 @Stateful
 public class StatefullSession implements StatefullSessionInterface {
 
-    String user;
+    UsersEntity user;
+
+
+    private LocalDate birthday;
 
     public StatefullSession(){
-        user = new String();
     }
 
-    public void addUser(String u) {
-        user=u;
+    public int getId(){
+        return user.getId();
     }
 
-    public String getUser() {
-        return user;
+    //firstname
+    public void setFirstname(String n) {
+        user.setFirstname(n);
     }
+
+    public String getFirstname() {
+        return user.getFirstname();
+    }
+
+    //gender
+    public void setGender(String n) {
+        user.setGender(n);
+    }
+
+    public String getGender() {
+        return user.getGender();
+    }
+
+    //email
+    public void setMail(String n) {
+        user.setEmail(n);
+    }
+
+    public String getMail() {
+        return user.getEmail();
+    }
+
+    //password
+    public void setPassword(String n) {
+        user.setPassword(n);
+    }
+
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    //lastname
+    public void setLastname(String n) {
+        user.setLastname(n);
+    }
+
+    public String getLastname() {
+        return user.getLastname();
+    }
+
+    //handle
+    public void setHandle(String n) {
+        user.setHandle(n);
+    }
+
+    public String getHandle() {
+        return user.getHandle();
+    }
+
+    //birthday
+    public void setHandle(LocalDate n) {
+        user.setBirthday(n);
+    }
+
+    public LocalDate getBirthday() {
+        return user.getBirthday();
+    }
+
+    //roles
+    public Set<RolesEntity> getRoles(){
+        return user.getRoles();
+    }
+
+    public void setRoles(Set<RolesEntity> n) {
+        user.setRoles(n);
+    }
+
+    //carts
+    public Set<CartsEntity> getCarts(){
+        return user.getCarts();
+    }
+
+    public void setCarts(Set<CartsEntity> n) {
+        user.setCarts(n);
+    }
+
+    //pokemons
+    public Set<PokemonsArticlesEntity> getPokemons(){
+        return user.getPokemons();
+    }
+
+    public void setPokemons(Set<PokemonsArticlesEntity> n) {
+        user.setPokemons(n);
+    }
+
+    //feedbacks
+    public Set<FeedbackEntity> getFeedbacks(){
+        return user.getFeedbacks();
+    }
+
+    public void setFeedbacks(Set<FeedbackEntity> n) {
+        user.setFeedbacks(n);
+    }
+
 }
--- ecom/ejbs/src/main/java/database/session/StatefullSessionInterface.java
+++ ecom/ejbs/src/main/java/database/session/StatefullSessionInterface.java
@@ -1,10 +1,38 @@
 package database.session;
 
+import database.entities.CartsEntity;
+import database.entities.FeedbackEntity;
+import database.entities.RolesEntity;
+import database.entities.articles.PokemonsArticlesEntity;
+
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 import javax.ejb.Remote;
 
 @Remote
 public interface StatefullSessionInterface {
-    void addUser(String user);
-    String getUser();
+    int getId();
+    void setFirstname(String n);
+    String getFirstname();
+    void setGender(String n);
+    String getGender();
+    void setMail(String n);
+    String getMail();
+    void setPassword(String n);
+    String getPassword();
+    void setLastname(String n);
+    String getLastname();
+    void setHandle(String n);
+    String getHandle();
+    void setHandle(LocalDate n);
+    LocalDate getBirthday();
+    Set<RolesEntity> getRoles();
+    void setRoles(Set<RolesEntity> n);
+    Set<CartsEntity> getCarts();
+    void setCarts(Set<CartsEntity> n);
+    Set<PokemonsArticlesEntity> getPokemons();
+    void setPokemons(Set<PokemonsArticlesEntity> n);
+    Set<FeedbackEntity> getFeedbacks();
+    void setFeedbacks(Set<FeedbackEntity> n);
 }
